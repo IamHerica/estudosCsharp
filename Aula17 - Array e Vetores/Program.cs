31c36bf9d74c1425c69db8dce53b6d72b6d13587
IamHerica <hericacadoni@gmail.com>
2021-05-20 20:53:05 -0300

    array

---
+++ Aula17 - Array e Vetores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula17
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] n = new int[5];
+            int[] num = new int[3] {66,77,88};
+            int[] array = {99, 111, 122, 133};
+            n[0] = 11;
+            n[1] = 22;
+            n[2] = 33;
+            n[3] = 44;
+            n[4] = 55;
+
+            Console.WriteLine(n[0]);
+            Console.WriteLine(num[2]);
+            Console.WriteLine(array[2]);
+
+
+        }
+    }
+}
