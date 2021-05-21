01bf4bd2d828db072901d102d89826d1dc00d338
IamHerica <hericacadoni@gmail.com>
2021-05-21 11:55:11 -0300

    comando for

---
+++ Aula19 - For/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula19
+{
+    class Program
+    {
+        static void Main()
+        {
+           int i;
+
+           for(i = 0; i <= 3; i++) { //i = 0; i < num.Length; i++ (pega o tamanho da variavel a preencher)
+               Console.WriteLine($"Loop rodando na {i}º vez.");
+           }
+        }
+    }
+}
