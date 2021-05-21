08decbe61c92cf497af53f905cbf0040337ca39b
IamHerica <hericacadoni@gmail.com>
2021-05-21 11:29:25 -0300

    matrizes

---
+++ Aula18 - Matrizes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula18
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] n = new int[2,2]; //3 linhas e 5 colunas
+
+            n[0,0] = 1;
+            n[0,1] = 2;
+            n[1,0] = 4;
+            n[1,1] = 5;
+    
+            Console.WriteLine(n[0,1]);
+
+        }
+    }
+}
