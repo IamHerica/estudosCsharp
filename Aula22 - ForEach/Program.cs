60862389e8b25f54ed83183c64ab4503cb4f9388
IamHerica <hericacadoni@gmail.com>
2021-05-25 11:08:32 -0300

    leitura de elementos de uma coleção

---
+++ Aula22 - ForEach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula22
+{
+    class Program
+    {
+        static void Main( )
+        {
+            int[] num = new int[3] {00, 11, 22};
+
+            /*for (int i = 0; i < num.Length; i++) {//o tamanho de num é 2, pois tem indice 0,1,2
+                Console.WriteLine(num[i]);
+            } 
+            */
+
+            foreach (int n in num) {
+                Console.WriteLine(n);
+            } // n é uma variavel de iteração
+        }
+    }
+}
