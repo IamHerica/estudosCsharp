ef9ce42628268380ceb4effd5459dc1a76c930da
IamHerica <hericacadoni@gmail.com>
2021-05-25 15:42:13 -0300

    passando valor com ponteiro

---
+++ Aula25 - Passagem por valor e referencia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aula25
+{
+    class Program
+    {
+        static void Main()
+        {
+            int v = 10;
+            //dobrar0(v); passagem por valor
+            dobrar(ref v); //passagem por referencia
+            Console.WriteLine(v);
+        }
+        static void dobrar(ref int valor) {
+            valor*=2;
+        }
+    }
+}
