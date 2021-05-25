5c8e122dcc2ebbc08d7ce6d4511908246ccbc6a5
IamHerica <hericacadoni@gmail.com>
2021-05-25 10:56:43 -0300

    rodar pelo menos uma vez

---
+++ Aula21 - DoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula21
+{
+    class Program
+    {
+        static void Main()
+        {
+            string senha = "123";
+            string senhauser;
+            int tentativas = 0;
+            
+
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Digite a senha:");
+                senhauser = Console.ReadLine();
+                tentativas+=1;
+
+            } while (senha!= senhauser);
+            Console.Write("Senha correta! Número de tentativas: {0}.", tentativas);
+        }
+    }
+}
