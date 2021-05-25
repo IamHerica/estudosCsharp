ded832d3a1e220b505351af4f3a810edb1503531
IamHerica <hericacadoni@gmail.com>
2021-05-25 15:28:14 -0300

    métodos

---
+++ Aula24 - Métodos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula24
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n1, n2, somado;
+
+            hello();
+
+
+            Console.Write("Digite o primeiro valor: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            somado = soma(n1, n2);
+            Console.Write($"{n1} + {n2} = {somado}");
+            // Console.Write($"{n1} + {n2} = {soma(n1, n2)}");
+        }
+
+        static void hello()
+        { //é estatico, o retorno é void e o nome do metodo é hello
+            Console.WriteLine("Hello pessoinha!");
+        }
+
+        static int soma(int n1, int n2)
+        {
+
+            int soma = n1 + n2;
+            return soma;
+        }
+    }
+}
