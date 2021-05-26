2dcd7b7b19cf2b9050ca9b099191170acd3539e0
IamHerica <hericacadoni@gmail.com>
2021-05-25 22:50:46 -0300

    saida out

---
+++ Aula26 - Argumento out/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula26
+{
+    class Program
+    {
+        static void Main()
+        {
+            int dividend, divis, quoci, rest;
+
+            dividend = 10;
+            divis = 3;
+            quoci = divide(dividend,divis, out rest);
+            Console.Write($"{dividend} / {divis} = {quoci}\nRestou: {rest}");
+
+        }
+        static int divide(int dividendo, int divisor, out int resto) { //out vai retornar o resto
+
+            int quociente;
+            quociente = dividendo / divisor;
+            resto = dividendo % divisor;
+            return quociente;
+        }
+    }
+}
