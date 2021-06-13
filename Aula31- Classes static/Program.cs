a94d5777fe845803d04f44157d83b030bade6242
IamHerica <hericacadoni@gmail.com>
2021-06-13 20:19:56 -0300

    static

---
+++ Aula31- Classes static/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Aula30{
+    static public class Jogador { //static nao pode ter construtor e nao permite instanciamento
+    //classes static precisam de membros static
+        static public int energy;
+        static public bool alive;
+        static public string nome; 
+        static public void iniciar(string n){ 
+            energy = 100;
+            alive = true;
+            nome = n;
+        }
+         static public void info()
+         {
+            Console.WriteLine($"Nome do jogador: {nome}");
+            Console.WriteLine($"Energia do jogador: {energy}");
+            Console.WriteLine($"Situação do jogador: {alive}\n");
+         }
+
+    }
+
+        class Inimigo {
+            static public bool alerta; //static para todos inimigos entrarem no mesmo estado de alerta
+            public string nome;
+            
+            public Inimigo(string n) {
+                alerta = false;
+                nome = n;
+            }
+
+            public void info(){
+                Console.WriteLine(nome);
+                Console.WriteLine(alerta);
+                Console.WriteLine("--------------------------");
+            }
+        }
+        class Program
+    {
+        static void Main()
+        {
+          
+        Jogador.iniciar("Bruno");
+        Jogador.info();
+
+        Inimigo i1 = new Inimigo("doido");
+        Inimigo i2 = new Inimigo("Maluco");
+        Inimigo i3 = new Inimigo("Pirado");
+
+        Inimigo.alerta=true; //mudar a situação do alerta para todos os inimigos
+
+        i1.info();
+        i2.info();
+        i3.info();
+
+            
+        }
+        
+    }
+
+    }
+    
+
---
+++ Aula33 - Public vs Private/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Aula33
+{
+
+    class Jogador {
+
+        private int energia;
+        private string nome;
+        public Jogador(string nome) {
+            this.nome = nome;
+            energia = 100;
+        }
+    //para obter valores private
+        public int getEnergia() { // get = obter
+            return energia;
+        }
+        public string getNome() {
+            return nome;
+        }
+
+        public void setEnergia(int e) {
+            if(e < 0){
+                if (energia + e < 0){ //caso entrar com numero negativo, resultar em + com - 
+                    energia = 0;
+                } else {
+                    energia +=e;
+                }
+            } else if(e > 0){
+                if (energia + e > 100){
+                    energia = 100;
+                } else {
+                    energia +=e;
+                }
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+           Jogador j1 = new Jogador("Bruno");
+
+           j1.setEnergia(-150);
+
+           Console.WriteLine($"Nome: {j1.getNome()}");
+           Console.WriteLine($"Energia: {j1.getEnergia()}");
+        }
+    }
+}
