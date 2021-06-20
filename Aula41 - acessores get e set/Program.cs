71fea2b49229914f52e0052295701807ee3c798f
IamHerica <hericacadoni@gmail.com>
2021-06-20 18:41:17 -0300

    velocidade do carro

---
+++ Aula41 - acessores get e set/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula41
+{
+
+    class Carro {
+        private int velMax;
+        public int vm {
+            get { //read only
+                return velMax;
+            }
+            set { //write only
+                if(value < 0)
+                    velMax = 0;
+                else if (value > 300)
+                    velMax = 300;
+                else 
+                    velMax = value;
+            }
+        }
+        public Carro(){
+            vm = 120;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+
+            Console.WriteLine($"Velocidade: {c1.vm}");
+        }
+    }
+}
