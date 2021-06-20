721dff77c9cfe4fc511928b4b28855ff2bce9d41
IamHerica <hericacadoni@gmail.com>
2021-06-20 15:39:31 -0300

    sealed

---
+++ Aula40 - Classe Sealed/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//sealed significa que a classe nao pode ser herdada
+namespace Aula40
+{
+    sealed class Veiculo {
+
+    }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
