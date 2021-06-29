3f298c6df9bb15c70ac1488e589a26d104591eae
Iamherica <hericacadoni@gmail.com>
2021-06-29 20:48:45 -0300

    variaveis static

---
+++ Aula49 - Métodos e Variavéis Static/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//quer utilizar os metodos de uma classe, mas nao precisa declarar um objeto dessa classe
+
+namespace Aula49
+{
+
+    class Mat{
+        public static double pi = 3.14;
+
+
+        public static dobro (int n) {
+            return n*2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double vpi = Mat.pi; //posso acessar pq a variavel é static
+            int num = 10;
+        
+            Console.WriteLine($"O dobro de {num} é:" + Mat.dobro(num));
+        }
+    }
+}
