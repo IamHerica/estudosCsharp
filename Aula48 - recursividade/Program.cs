018a8fa2ffb2be1c32093cf1867a4dd97e6a246c
Iamherica <hericacadoni@gmail.com>
2021-06-29 20:31:02 -0300

    fatorial

---
+++ Aula48 - recursividade/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+//funcao que chama ela mesma
+
+namespace Aula48
+{
+    class Calc{
+
+        public int soma(params int[]n) {
+
+            int soma = 0;
+            for(int i = 0; i < n.Length; i++) {
+                soma+=n[i];
+            }
+            return soma;
+        }
+
+         public double soma(params double[] n) { //sobrecarga
+            double soma = 0;
+            for(int i = 0; i < n.Length; i++) {
+                soma+=n[i];
+            }
+            return soma;
+        }
+
+        public int fatorial(int n) {
+            int resultado;
+            
+            if(n <=1 ) {
+                resultado=1;
+            } else {
+               resultado = n* fatorial(n-1);
+            }
+            return resultado;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Calc calculadora = new Calc();
+
+            int resultadoFat = calculadora.fatorial(10);
+
+            System.Console.WriteLine(resultadoFat);
+        }
+    }
+}
