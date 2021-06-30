b123ba3eeff47201828710350eac041b9aee8794
Iamherica <hericacadoni@gmail.com>
2021-06-29 21:15:14 -0300

    delegates

---
+++ Aula50 - Delegates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+//Delegate é um objeto que pode fazer referencia a metodos dentro da classe
+
+
+namespace Aula50
+{
+
+delegate int Op(int n1, int n2);
+    class Mat {
+        public static int soma(int n1, int n2) {
+            return n1 + n2;
+        }
+
+        public static int mult(int n1, int n2) {
+            return n1 *n2;
+        }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado;
+
+            Op d1 = new Op(Mat.soma);
+
+            resultado = d1(10, 50);
+            Console.WriteLine("Soma:" + resultado);
+
+            d1 = new Op(Mat.mult); //como mat é static, precisou referenciar
+
+            resultado= d1(10, 50);
+            Console.WriteLine("Multiplicação:" + resultado);
+
+
+        }
+    }
+}
