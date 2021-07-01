313007fdbd6332986659d9ff831e8d7ccb0240f9
Iamherica <hericacadoni@gmail.com>
2021-07-01 15:55:17 -0300

    editar

---
+++ Aula51 - Argumentos de entrada do programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
