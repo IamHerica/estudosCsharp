6e2ebb86029d40263e74ea1a41c691202c900934
Iamherica <hericacadoni@gmail.com>
2021-07-01 15:55:40 -0300

    exceções

---
+++ Aula52 - Try Catch Finally Parte 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, res;
+            n1=n2=res=0;
+            n1 = 10;
+             n2 = 0;
+        try
+        {
+        res = n1/n2; 
+            Console.WriteLine($"{n1}/{n2} = {res}");
+
+        }
+        catch (Exception e)
+        {
+        System.Console.WriteLine("Error! " + e.Message);
+        }
+                
+            
+        }
+    }
+}
